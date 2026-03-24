1cfa00b9fee40f9d94443cdae3b9b31831d691ce
i_mikhailov <i_mikhailov@tochka.com>
2026-03-24 17:22:19 +0500

    5 done

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -1,23 +1,29 @@
 const express = require('express');
 const router = express.Router();
 
+users = {
+  items: [
+    {
+      id: 1,
+      name: 'Ivan'
+    },
+    {
+      id: 2,
+      name: 'Darya'
+    }
+  ]
+}
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
 
-  obj = {
-    items: [
-      {
-        id: 1,
-        name: 'Ivan'
-      },
-      {
-        id: 2,
-        name: 'Darya'
-      }
-    ]
-  }
-
-  res.send(obj);
+  res.send(users);
 });
+
+router.post('/', function(req, res) {
+  users.items.push(req.body);
+  res.status(201).json(req.body);
+  res.send(users);
+})
 
 module.exports = router;
